619ed7677cf24239ce2eadab087daeb526b9a4d8
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-05-20 19:54:31 +0200

    - Added help command

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+helpFile = open('assets/help.txt', 'r')
+helpText = helpFile.readlines()
 
 
-@client.event
-async def on_ready():
-    print("Connected to Linux/BSD Gaming Discord!")
+class BinBashBotClient(discord.Client):
+    async def on_ready(self):
+        print("Connected to Linux/BSD Gaming Discord!")
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith("bbb help"):
+            for line in helpText:
+                await message.author.send(line)
+            return
+        if message.content.startswith("bbb"):
+            await message.channel.send("I am a slave of your will. If you need help simply type bbb help and I'll "
+                                       "immediately serve you!")
+            return
+
+
+client = BinBashBotClient()
 client.run(TOKEN)
